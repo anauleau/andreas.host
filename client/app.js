c276f5d872f310ed825f0d409ddfe3c16d852a03
Andreas Nauleau <anauleau@prosper.com>
2017-03-26 17:05:22 -0700

    switches to use imgr for speed

--- client/app.js
+++ client/app.js
@@ -2,15 +2,11 @@ import 'script-loader!jquery'
 import 'script-loader!foundation-sites'
 import 'foundation-sites/dist/css/foundation.css'
 import './app.css'
-const landingImageSource = require('file-loader!./assets/designDre.jpg')
 const {art} = require('./assets/ascii-art')
 
 export {onLoad}
 
 function onLoad () {
-  let landingImage = document.getElementById('landing-image')
-  landingImage.src = landingImageSource
-
   console.log(art)
   console.log('email me: anauleau@gmail.com')
 }
